5b9286ad32e562923c868ceee1f9e17c7fc14614
envyusone <abriche123@gmail.com>
2026-03-16 18:15:17 -0600

    added people

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,35 @@
+import { useState } from "react";
+
+const dummyGuests = [
+  { id: 1, name: "Edmond Steuber", email: "Edmond.Steuber@example.com" },
+  { id: 2, name: "Nichole Luettgen", email: "Nichole_Luettgen43@example.org" },
+  { id: 3, name: "Idell Mayert", email: "Idell_Mayert@example.org" },
+];
+
 export default function App() {
-  return <></>;
+  const [guests, setGuests] = useState(dummyGuests);
+
+  return (
+    <div className="App">
+      <h1>Guest List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {guests.map((guest) => {
+            return (
+              <tr key={guest.id}>
+                <td>{guest.name}</td>
+                <td>{guest.email}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
